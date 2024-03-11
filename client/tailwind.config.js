ee111a9afdb92c6adaeef5b9a45fc52a3ecf3de1
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-03-11 12:24:34 -0400

    Update postcss-import to version 16.0.1

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
+  content: [
+    './index.html', './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   purge: [],
-  darkMode: media, 
+  darkMode: false, 
   theme: {
     extend: {},
   },

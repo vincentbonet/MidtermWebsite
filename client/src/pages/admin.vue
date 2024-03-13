e36aa30cbd9d83d315ec846d44491ad708a784d3
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-03-13 14:46:18 -0400

    Refactor user grid layout in admin page

--- client/src/pages/admin.vue
+++ client/src/pages/admin.vue
@@ -34,21 +34,21 @@ export default {
 </script>
 
 <template>
-<div class ="flex items-center justify-center h-screen">
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-    <div v-for="(user, index) in users" :key="index" class="bg-white p-4 shadow-md rounded-md">
-      <img :src="user.picture" :alt="`${user.firstName} ${user.lastName}'s Profile Picture`" class="w-16 h-16 rounded-full mx-auto mb-4">
-      <div class="text-center">
-        <p class="text-lg font-semibold">{{ user.firstName }}</p>
-        <p class="text-lg font-semibold">{{ user.lastName }}</p>
-        <p class="text-gray-500">{{ user.email }}</p>
-        <p class="text-gray-500">{{ user.handle }}</p>
-        <p class="text-gray-500">{{ user.isAdmin ? 'Administrator' : 'Regular User' }}</p>
+    <div class="flex items-center justify-center min-h-screen">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto max-w-screen-lg">
+        <div v-for="(user, index) in users" :key="index" class="bg-white p-4 shadow-md rounded-md">
+          <img :src="user.picture" :alt="`${user.firstName} ${user.lastName}'s Profile Picture`" class="w-16 h-16 rounded-full mx-auto mb-4">
+          <div class="text-center">
+            <p class="text-lg font-semibold">{{ user.firstName }}</p>
+            <p class="text-lg font-semibold">{{ user.lastName }}</p>
+            <p class="text-gray-500">{{ user.email }}</p>
+            <p class="text-gray-500">{{ user.handle }}</p>
+            <p class="text-gray-500">{{ user.isAdmin ? 'Administrator' : 'Regular User' }}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-</template>
+  </template>
 
 <style scoped>
 </style>

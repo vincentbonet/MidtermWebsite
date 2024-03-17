05821c7cdb5170dda994ca0781c0699682919803
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-03-17 13:55:32 -0400

    Update vue-router version

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,9 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from 'vue-router/auto-routes'
+import { createRouter, createWebHistory } from 'vue-router';
+import home from '../pages/home.vue';
+import admin from '../pages/admin.vue';
+import friendsactivity from '../pages/friendsactivity.vue';
+import myactivity from '../pages/myactivity.vue';
+import statistics from '../pages/statistics.vue';
+import peoplesearch from '../pages/peoplesearch.vue';
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: home
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: admin
+  },
+  {
+    path: '/friendsactivity',
+    name: 'friendsactivity',
+    component: friendsactivity
+  },
+  {
+    path: '/myactivity',
+    name: 'myactivity',
+    component: myactivity
+  },
+  {
+    path: '/statistics',
+    name: 'statistics',
+    component: statistics
+  },
+  {
+    path: '/peoplesearch',
+    name: 'peoplesearch',
+    component: peoplesearch
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: routes
+  history: createWebHistory(),
+  routes
 })
 
-export default router
+export default router;

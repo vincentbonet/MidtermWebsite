5c4eaef07a9a66987d1464b92fbf56561dc69bce
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-03-17 18:15:06 -0400

    Add store to main.ts

---
+++ client/src/store/index.ts
@@ -0,0 +1,26 @@
+// store/index.ts
+import { createStore, Store } from 'vuex';
+
+interface RootState {
+  isLoggedInAsRobert: boolean;
+}
+
+const store: Store<RootState> = createStore<RootState>({
+  state: {
+    isLoggedInAsRobert: false,
+  },
+  mutations: {
+    loginAsRobert(state) {
+      state.isLoggedInAsRobert = true;
+    },
+    logout(state) {
+      state.isLoggedInAsRobert = false;
+    },
+  },
+  actions: {
+  },
+  modules: {
+  },
+});
+
+export default store;

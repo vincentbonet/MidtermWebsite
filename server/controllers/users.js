00b1051c07abeb7f02b336a2866368f2efe923ea
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-02 11:53:03 -0400

    started server work

---
+++ server/controllers/users.js
@@ -0,0 +1,118 @@
+/* B"H
+*/
+const users = require('../models/users')
+const express = require('express');
+const app = express.Router();
+
+/*
+    Ways that we send information to the server:
+    1. Path parameters
+    2. Query parameters
+    3. Body
+    4. Headers
+*/
+
+/** 
+ * @typedef {import('../../client/src/model/users').User} User 
+ * @typedef {import('../../client/src/model/transportTypes').DataEnvelope<User> } UserDataEnvelope
+ * @typedef {import('../../client/src/model/transportTypes').DataListEnvelope<User> } UserDataListEnvelope
+ * */
+
+app
+    .get('/', (req, res, next) => {
+        users.getAll()
+        .then(all => {
+            /** @type { UserDataListEnvelope } */
+            const response = {
+                data: all,
+                totalCount: all.length,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
+        
+    })
+    .get('/search', (req, res, next) => {
+
+        const search = req.query.q;
+        if(typeof search !== 'string' ) throw new Error('search is required');
+        users.search(search)
+        .then(result => {
+            /** @type { UserDataListEnvelope } */
+            const response = {
+                data: result,
+                totalCount: result.length,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
+    })
+    .get('/:id', (req, res, next) => {
+        const id = req.params.id;
+        users.get(+id)
+        .then(result => {
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
+    })
+    .post('/', (req, res, next) => {
+        const user = req.body;
+        console.log("1: About to add user");
+        users.add(user)
+        .then(result => {
+            console.log("5: Returned from add user");
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
+    })
+    .post('/login', (req, res, next) => {
+        const { email, password } = req.body;
+        users.login(email, password)
+        .then(result => {
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
+    })
+    .patch('/:id', (req, res, next) => {
+        const user = req.body;
+        user.id = req.params.id;
+        users.update(user)
+        .then(result => {
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
+
+            res.send(response);
+        }).catch(next);
+    })
+    .delete('/:id', (req, res, next) => {
+        const id = req.params.id;
+        users.remove(+id)
+        .then(result => {
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
+    })
+
+
+
+
+module.exports = app

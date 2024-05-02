1b886cb358bdd1e902be4e8ffba4cccd4149bb09
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-02 12:08:18 -0400

    Refactor user files for data and model

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,16 +1,7 @@
-/* B"H
-*/
 const users = require('../models/users')
 const express = require('express');
 const app = express.Router();
 
-/*
-    Ways that we send information to the server:
-    1. Path parameters
-    2. Query parameters
-    3. Body
-    4. Headers
-*/
 
 /** 
  * @typedef {import('../../client/src/model/users').User} User 
--- server/models/users.js
+++
@@ -1,1 +0,0 @@
-// fill with model component of the server

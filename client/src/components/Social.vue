fba99be3ac1c586f716af4a62eb76d211be61078
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-05 17:14:41 -0400

    chore: Update server configuration and routing for static files and API endpoints

---
+++ client/src/components/Social.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import userData from '@/assets/users.json';
+import { ref } from 'vue';
+
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped>
+</style>
--- server/index.js
+++ server/index.js
@@ -1,19 +1,42 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const userController = require('./controllers/users.js');
+const activityController = require('./controllers/activity.js');
+const exerciseController = require('./controllers/exercise.js');
+const { parseAuthorizationToken, requireUser } = require('./middleware/auth.js');
 
 const app = express();
+
 const PORT = process.env.PORT ?? 3000; 
 
 app
-    .use(express.static('client/dist')) 
+    .use('/', express.static(path.join(__dirname, '../client/dist')))
+
     .use(express.json())
+
     .use((req, res, next) => { 
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', '*');
         res.setHeader('Access-Control-Allow-Headers', '*');
+        if(req.method === 'OPTIONS') {
+            return res.sendStatus(200);
+        }
         next();
     }) 
+
+    .use(parseAuthorizationToken)
+
+    .use('/users', userController)
+
+    .use('/activity', activityController)
+
+    .use('/exercise', exerciseController)
+
+    .get('*', (req, res) =>  {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+})
+
 
 // 404 
 app.use((req, res, next) => {

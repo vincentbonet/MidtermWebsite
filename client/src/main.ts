8ee215dbbc51f12eddd50aa768dc2718a513ab55
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-07 17:34:54 -0400

    refactor: Remove unnecessary files and dependencies

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,13 +4,11 @@ import 'vue-toastification/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import Toaster from 'vue-toastification'
 
 const app = createApp(App)
 
 app
-    .use(store)
     .use(router)
     .use(Toaster, {})
 

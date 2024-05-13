4e2128cc178a4c1675b14ffc564f3ca2912e7717
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-13 12:07:28 -0400

    refactor: Update import paths for auth middleware and myfetch in NavBar and session.ts

--- client/src/components/AddWorkout.vue
+++ client/src/components/AddWorkout.vue
@@ -1,69 +1,51 @@
 <template>
-  <div class="flex items-center justify-center h-screen">
-    <div class="bg-white max-w-7xl px-6 lg:px-8">
-      <div class="mb-4">
-        <label for="name" class="text-base leading-7 text-gray-900">Name:</label>
-        <input v-model="newWorkout.name" id="name" type="text" class="px-4 py-2 border rounded-md">
-      </div>
-      <div class="mb-4">
-        <label for="distance" class="text-base leading-7 text-gray-900">Distance (miles):</label>
-        <input v-model="newWorkout.distance" id="distance" type="number" step="0.1" class="px-4 py-2 border rounded-md">
-      </div>
-      <div class="mb-4">
-        <label for="duration" class="text-base leading-7 text-gray-900">Duration (hours:min):</label>
-        <input v-model="newWorkout.duration" id="duration" type="text" class="px-4 py-2 border rounded-md">
-      </div>
-      <button @click="addWorkout" class="mb-4 px-4 py-2 bg-blue-500 text-white rounded-md">Add Workout</button>
-      <div class="text-center">
-        <div v-for="(workout, index) in workouts" :key="index" class="mb-8">
-          <dt class="text-base leading-7 text-gray-900">{{ `Stats for ${workout.name} today:` }}</dt>
-          <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">{{ `${workout.distance} miles` }}</dd>
-          <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">{{ `${workout.duration} duration (hours:min)` }}</dd>
-        </div>
-      </div>
+  <div>
+    <div class="mb-4">
+      <label for="name" class="text-base leading-7 text-gray-900">Name:</label>
+      <input v-model="newActivity.name" id="name" type="text" class="px-4 py-2 border rounded-md">
     </div>
+    <div class="mb-4">
+      <label for="distance" class="text-base leading-7 text-gray-900">Distance (miles):</label>
+      <input v-model="newActivity.distance" id="distance" type="number" step="0.1" class="px-4 py-2 border rounded-md">
+    </div>
+    <div class="mb-4">
+      <label for="duration" class="text-base leading-7 text-gray-900">Duration (hours:min):</label>
+      <input v-model="newActivity.duration" id="duration" type="text" class="px-4 py-2 border rounded-md">
+    </div>
+    <button @click="submitForm" class="mb-4 px-4 py-2 bg-blue-500 text-white rounded-md">Add Activity</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { api } from '../viewModel/session';
-import type { Activity } from '../model/activites';
 
-interface Workout {
-  name: string;
-  distance: number;
-  duration: string;
-}
+const newActivity = ref({
+  name: '',
+  distance: null,
+  duration: '',
+});
 
+const submitForm = async () => {
+  if (newActivity.value.name && newActivity.value.distance !== null && newActivity.value.duration) {
+    try {
+      const response = await fetch('/api/v1/activities', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newActivity.value),
+      });
 
-    const workouts = ref<Activity[]>([]);
-    const newWorkout = ref<Workout>({
-      name: '',
-      distance: 0,
-      duration: '',
-    });
-
-    const addWorkout = async () => {
-      if (newWorkout.value.name && newWorkout.value.distance !== null && newWorkout.value.duration) {
-        try {
-          const data = await api<Activity>('activity', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newWorkout.value),
-          });
-
-          workouts.value.unshift(data);
-          newWorkout.value = { name: '', distance: 0, duration: '' };
-        } catch (error) {
-          console.error('There has been a problem with your fetch operation:', error);
-        }
-      } else {
-        alert('Please fill in all workout details.');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-    };
 
-
+      newActivity.value = { name: '', distance: null, duration: '' };
+    } catch (error) {
+      console.error('There has been a problem with your fetch operation:', error);
+    }
+  } else {
+    alert('Please fill in all activity details.');
+  }
+};
 </script>
--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -6,10 +6,7 @@
     </footer>
   </template>
   
-  <script setup lang = "ts">
-  export default {
-    name: 'Footer'
-  }
+  <script setup lang="ts">
   </script>
   
   <style scoped>

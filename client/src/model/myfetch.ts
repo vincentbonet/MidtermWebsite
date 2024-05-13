9e2a2d634ecbfb8a7e5775aa3ccc0d835a42b263
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-13 15:59:42 -0400

    more changes, not done yet, need to figure out fetch error in myfetch.ts

--- client/src/model/myfetch.ts
+++ client/src/model/myfetch.ts
@@ -1,45 +1,21 @@
 import type { DataEnvelope } from "./transporttypes";
-import { useToast } from "vue-toastification";
 
-const toast = useToast();
 export const API_ROOT = import.meta.env.VITE_API_ROOT;
 
-export function rest(url: string, data?: unknown, method?: string) {
+export function rest(url: string, data?: unknown, method?: string, headers?: Record<string, string>){
     return fetch(url, {
         method: method ?? (data ? "POST" : "GET"),
         headers: {
             "Content-Type": "application/json",
+            ...headers,
         },
         body: data ? JSON.stringify(data) : undefined,
     })
-    .then(response => {
-        console.log("response", response);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        // Check if the response is JSON
-        const contentType = response.headers.get("content-type");
-        if (contentType && contentType.indexOf("application/json") !== -1) {
-            return response.json();
-        } else {
-            // Throw an error with a custom message for non-JSON responses
-            throw new Error("Unexpected response format. Expected JSON.");
-        }
-    })
-    .catch(error => {
-        console.error("Fetch error:", error);
-        throw error; 
-    });
+    .then(x => x.json())
 }
-
 
 export function api<T>(action: string, data?: unknown, method?: string): Promise<DataEnvelope<T>> {
     return rest(`${API_ROOT}/${action}`, data, method);
-}
-
-export function handleError(error: Error) {
-    console.error("API error:", error);
-    toast.error(error.message || error);
 }
 
 export function refSession() {

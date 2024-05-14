94fd9e81afc60822c49009c7b73b829ef9338c82
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-14 19:52:28 -0400

    refactor: Update rest.ts to remove unused code and improve code readability

--- client/src/model/rest.ts
+++ client/src/model/rest.ts
@@ -2,34 +2,17 @@ import type { DataEnvelope } from "./transporttypes";
 
 export const API_ROOT = import.meta.env.VITE_API_ROOT;
 
-
-export function rest(url: string, data?: unknown, method?: string, headers?: Record<string, string>){
+export function rest(url: string, data?: unknown, method?: string) {
     return fetch(url, {
         method: method ?? (data ? "POST" : "GET"),
         headers: {
             "Content-Type": "application/json",
-            ...headers,
         },
         body: data ? JSON.stringify(data) : undefined,
     })
     .then(x => x.json())
 }
 
-export function api<T>(action: string, data?: unknown, method?: string): Promise<DataEnvelope<T>>{
+export function api<T>(action: string, data?: unknown, method?: string): Promise<DataEnvelope<T>> {
     return rest(`${API_ROOT}/${action}`, data, method);
-}
-
-export function loadScript(url: string, id: string){
-    return new Promise<any>((resolve, reject) => {
-        if(document.getElementById(id)){
-            resolve(null);
-            return;
-        }
-        const script = document.createElement("script");
-        script.src = url;
-        script.id = id;
-        script.onload = resolve;
-        script.onerror = reject;
-        document.body.appendChild(script);
-    });
 }

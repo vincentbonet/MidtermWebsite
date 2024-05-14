56b70e0123201774efb78522cd58dc5cfe2c55f5
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-14 17:09:14 -0400

    refactor: Update CORS handling and static file serving in index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const usersRouter = require('./controllers/users');
@@ -6,70 +5,52 @@ const activityRouter = require('./controllers/activities');
 const exerciseRouter = require('./controllers/exercises');
 const { parseAuthToken } = require('./middleware/auth');
 
-/**  
- * @typedef {import('../client/src/model/transporttypes').DataEnvelope<null> } ErrorDataEnvelope
- * */
-
-//Intializing the express app
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app
-
- //serving the static files from the client
- .use(express.static('../client/dist'))
-
-
- //CORS handling
- .use(express.json())
- .use((req, res, next) => { 
+  // CORS handling
+  .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
-    if(req.method === 'OPTIONS') {
-        return res.sendStatus(200);
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
     }
     next();
-})
-app
-    .get('/', (req, res) => {
-        res.send('Hello Robert!');
-    })
-    //User routes
-    .use('/api/v1/users', usersRouter)
-    //Activity routes
-    .use('/api/v1/activities', activityRouter)
-    //Exercise routes
-    .use('/api/v1/exercises', exerciseRouter)
-
-    //404 
-    .use((req, res) => {
-        res.sendFile(path.join(__dirname,  '../client/dist/index.html'));
-    })
-
-    // Error handling
-    .use((err, req, res, next) => {
+  })
+  // JSON body parsing middleware
+  .use(express.json())
+  // Static files serving
+  .use(express.static('../client/dist'))
+  // API routes
+  .use('/api/v1/users', usersRouter)
+  .use('/api/v1/activities', activityRouter)
+  .use('/api/v1/exercises', exerciseRouter)
+  // Protected route
+  .get('/protected-route', parseAuthToken, (req, res) => {
+    res.json({ message: 'This is a protected route' });
+  })
+  // 404 handler
+  .use((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  })
+  // Error handling
+  .use((err, req, res, next) => {
     console.error(err);
-    /** @type {ErrorDataEnvelope } */
-    const results = { 
+    const results = {
       isSuccess: false,
       message: err.message || 'Internal Server Error',
       data: null,
-     };
+    };
     res.status(500).send(results);
-    })
+  })
+  // Catch all route for Vue
+  .get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 
-    //Protected routes 
-    .get('/protected-route', parseAuthToken, (req, res) => {
-        res.json({ message: 'This is a protected route' });
-    })
-
-    //Catch all route for Vue
-    .get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-
-    //Start the server  
+// Start the server
 app.listen(PORT, () => {
-    console.log(`App is listening on http://localhost:${PORT}`);
+  console.log(`App is listening on http://localhost:${PORT}`);
 });

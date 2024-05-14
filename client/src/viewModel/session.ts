4756b937aa32bb99813449094a5a046d29275833
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-14 19:52:04 -0400

    refactor: Move session-related functions to separate file

--- client/src/viewModel/session.ts
+++ client/src/viewModel/session.ts
@@ -7,43 +7,46 @@ import { useToast } from "vue-toastification";
 const session = reactive({
     user: null as User | null,
     isLoading: 0,
-});
+})
 
-export function useLogin() {
+export const refSession = () => session;
+
+export function getSession(){
+    return session;
+}
+
+export function useLogin(){
     const router = useRouter();
     return {
         async login(user: User) {
             const x = await api<User>("users/login", user);
-            if (x) {
+            if(x){
                 session.user = x.data;
                 router.push("/");
             }
         },
-        async logout() {
-            await api("users/logout");
+        logout() {
             session.user = null;
             router.push("/");
-        },
-    }
+        }
+    };
 }
-
-export const refSession = () => session;
 
 export function api<T>(action: string, data?: unknown, method?: string) {
     session.isLoading++;
     return rest.api<T>(action, data, method)
-        .then(x => {
-            if (!x.isSuccess) {
-                showError(x);
-            }
-            return x;
-        })
-        .catch(showError)
-        .finally(() => session.isLoading--);
+    .then(x => {
+        if (!x.isSuccess){
+        }
+        return x;
+    })
+    .finally(() => session.isLoading--);
 }
 
 const toast = useToast();
+
 export function showError(error: any) {
     console.error(error);
+    
     toast.error(error.message || error);
 }

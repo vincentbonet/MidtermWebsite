c553fe1ca0b9d279e1cacf38edc5c8c829086c06
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-15 12:45:24 -0400

    refactor: Update rest function to include optional headers parameter

--- client/src/model/rest.ts
+++ client/src/model/rest.ts
@@ -2,15 +2,22 @@ import type { DataEnvelope } from "./transporttypes";
 
 export const API_ROOT = import.meta.env.VITE_API_ROOT;
 
-export function rest(url: string, data?: unknown, method?: string) {
+export async function rest(url: string, data?: unknown, method?: string, headers?: Record<string, string>){
     return fetch(url, {
         method: method ?? (data ? "POST" : "GET"),
         headers: {
             "Content-Type": "application/json",
+            ...headers,
         },
         body: data ? JSON.stringify(data) : undefined,
     })
-    .then(x => x.json())
+    .then(async response => {
+        if (response.ok) {
+            return await response.json();
+        } else {
+            throw await response.json();
+        }
+    });
 }
 
 export function api<T>(action: string, data?: unknown, method?: string): Promise<DataEnvelope<T>> {

5e7f481bb7317dd3e7c995808ffb967de496e179
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2024-05-15 13:59:37 -0400

    refactor: Update file paths for serving static files and data

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <footer class="bg-gray-800 text-white py-4">
     <div class="container mx-auto px-4 flex justify-center">
-      <a href="facebook.com" class="mr-4">
+      <a href="https://facebook.com" class="mr-4">
         <i class="fab fa-facebook"></i>
       </a>
-      <a href="twitter.com" class="mr-4">
+      <a href="https://twitter.com" class="mr-4">
         <i class="fab fa-twitter"></i>
       </a>
-      <a href="instagram.com">
+      <a href="https://instagram.com">
         <i class="fab fa-instagram"></i>
       </a>
     </div>
@@ -22,6 +22,6 @@
 
 <style scoped>
 footer {
-  background-color: #363736;
+  background-color: #111111;
 }
 </style>
--- client/src/pages/statistics.vue
+++ client/src/pages/statistics.vue
@@ -3,12 +3,12 @@ import Footer from '../components/Footer.vue'
 </script>
 
 <template>
-  <div class="flex items-center justify-center h-screen bg-gray-800">
+  <header class="flex items-center justify-center h-screen bg-gray-800">
       <div class="text-center">
         <h1 class="text-3xl font-semibold text-gray-100 mb-4">Extra Credit</h1>
         <h2 class="text-lg text-gray-200 mb-8">You don't need to provide a comprehensive statistics page, the home page will do.</h2>
       </div>
-    </div>
+    </header>
     <Footer></Footer>
 </template>
 
